71a2f5880e8f02002da67aefcadc8cf7d35823b6
pramudyadf <pramudyade04@gmail.com>
2022-03-13 18:06:52 +0700

    menambahkan file variabel.php

---
+++ variabel.php
@@ -0,0 +1,16 @@
+<?php
+$nilai1 = 8;
+$nilai2 = 5;
+
+//penjumlahan
+$hasilJumlah = $nilai1 + $nilai2;
+echo "hasil penjumlahan : $hasilJumlah </br>"; //hasil penjulahan
+
+//pengurangan
+$hasilKurang = $nilai1 - $nilai2;
+echo "hasil pengurangan : $hasilKurang </br>"; //hasil pengurangan
+
+//perkalian
+$hasilKali = $nilai1 * $nilai2;
+echo "hasil perkalian : $hasilKali </br>"; //hasil perkalian
+?>
